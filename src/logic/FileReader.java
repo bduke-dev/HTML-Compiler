c029175f00ca7b6e90a9652696d37823ece1f15d
Brandon Duke <bmduke1997@gail.com>
2017-05-18 21:46:50 -0400

    rename files

---
+++ src/logic/FileReader.java
@@ -0,0 +1,15 @@
+package logic;
+
+/**
+ * Created by brandon on 5/18/17.
+ */
+public class FileReader {
+    String headerHTML, footerHTML;
+
+    public FileReader(String headerHTML, String footerHTML){
+        this.headerHTML = headerHTML;
+        this.footerHTML = footerHTML;
+    }
+
+
+}
