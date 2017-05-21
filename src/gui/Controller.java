adce25a29c0ccedaef3fc5cedbe3053d67b2e354
Brandon Duke <bmduke1997@gail.com>
2017-05-21 19:48:11 -0400

    start fx, but its broken

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,4 +1,27 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+
+import java.awt.*;
+import java.io.File;
+
 public class Controller {
+    File navHTML, footerHTML, pathHTML;
+
+    private @FXML GridPane gp;
+    //private Stage primaryStage = (Stage) gp.getScene().getWindow();
+    private @FXML Label outputLogLabel;
+
+    public void initialize(){
+        outputLogLabel.setText("WORK");
+    }
+
+
+    public void getNavDirectory(){
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Choose Nav Directory");
+        //navHTML = chooser.showDialog(primaryStage);
+    }
 }
